1e040bbf86aea604b094567a4e625acd4d3facd3
Matthew Lockdall <mlockdall26@gmail.com>
2020-01-28 17:35:27 -0600

    updated whitelist

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(express.json());
 app.use(cookieParser(process.env.SECRET));
 
 app.use(cors({
-  origin: "http://localhost:3001",
+  origin: "http://localhost:3001, https://diabetes-supplies-portal.herokuapp.com",
   methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
   allowedHeaders: ['Content-Type', 'credentials', 'Accept', 'authorization' ],
   credentials: true,
