56a918654ebd33b5fd54b4de5f427645ab3a4722
Matthew Lockdall <mlockdall26@gmail.com>
2020-02-02 17:53:56 -0600

    removed async await from login

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -55,14 +55,14 @@ router.post("/create", async (req, res) => {
   })
 });
 
-  router.post("/login", async (req, res) => {
-    const user = await User.findOne({email: req.body.email});
+  router.post("/login", (req, res) => {
+    const user = User.findOne({email: req.body.email});
     if (!user) {
       return res.status(400).send('Email or Password is incorrect')
     }
     // console.log(user);
     
-   const validPass = await bcrypt.compare(req.body.password, user.password);
+   const validPass = bcrypt.compare(req.body.password, user.password);
    if (!validPass) {
      return res.status(400).send('Email or Password is incorrect');
    }
